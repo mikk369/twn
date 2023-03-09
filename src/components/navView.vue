1d3bcffc372c2eda3a19003844146de3d46422d4
mikk369 <mikk.ereline@vikk.ee>
2023-03-09 12:41:59 +0200

    added logo, menu-button

--- src/components/navView.vue
+++ src/components/navView.vue
@@ -1,13 +1,19 @@
 <template>
   <nav class="navbar">
-    <div class="navbar-toggle" id="mobile-menu">
-      <span class="bar">--</span>
-      <span class="bar">--</span>
-      <span class="bar">--</span>
+    <div class="nav-wrapper">
+      <div class="navbar-toggle" id="mobile-menu">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </div>
+      <div class="logo">
+        <img src="/logo.svg" alt="twn-logo" />
+      </div>
+      <div class="nav-space"></div>
     </div>
   </nav>
 </template>
-<style>
+<style scoped>
 /*  NAV STYLES */
 nav {
   display: none;
@@ -18,7 +24,30 @@ nav {
     grid-column: 1 / 3;
   }
 }
-.bar {
-  color: #3a3d57;
+.navbar {
+  padding: 10px 20px 10px 20px;
+}
+.nav-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.logo {
+  margin: 0;
+}
+.logo img {
+  width: 103px;
+  height: auto;
+}
+
+.navbar-toggle .bar {
+  width: 20px;
+  height: 3px;
+  margin: 3px auto;
+  transition: all 0.3s ease-in-out;
+  background-color: #3a3d57;
+  display: block;
+  cursor: pointer;
 }
 </style>
